e3eefe761d23c27803a380fbb4bc6065a71dca9c
alphajuliet <andrew@alphajuliet.com>
2018-12-15 17:44:09 +1100

    Added basic unit tests.

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,28 @@
 module Test.Main where
 
 import Prelude
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+{-- import Effect.Console (log) --}
+
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+import Chordal
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = runTest do
+  suite "Unit tests" do
+
+     test "Sanity check" do
+        Assert.assert "2+2=4" $ (2 + 2) == 4
+
+     test "Conversion functions" do
+        Assert.assert "noteToNum" $ (noteToNum "D" allNotes) == Just 2
+        Assert.assert "noteToNum" $ (noteToNum "H" allNotes) == Nothing
+        Assert.assert "numToNote" $ (numToNote 2 allNotes) == Just ["D"]
+        Assert.assert "numToNote" $ (numToNote 14 allNotes) == Just ["D"]
+
+
+-- The End
