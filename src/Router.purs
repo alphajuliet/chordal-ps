078fa8cb7ef35ebd82a71a9a51de64fcd6fe4824
alphajuliet <andrew@alphajuliet.com>
2019-01-20 20:16:43 +1100

    Implement optional parameters for chord and scale API calls. Comment out transposeNotes functionality for now. To be reviewed.

--- src/Router.purs
+++ src/Router.purs
@@ -9,10 +9,14 @@ module Router
 import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
+import Data.Int (floor, fromString)
+import Data.Map (Map, lookup, empty) as M
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Foldable (oneOf)
+import Data.String (split, Pattern(..)) as S
 import Routing (match)
-import Routing.Match (Match, root, lit, int, str, end)
+import Routing.Match (Match, root, lit, str, params, end, optionalMatch)
 import Chordal (allNotes, allChords, getChord, allScales, getScale, transposeNotes, Options) as C
 
 -- -------------------------------
@@ -21,57 +25,72 @@ import Chordal (allNotes, allChords, getChord, allScales, getScale, transposeNot
 type NoteName = String
 type ChordName = String
 type ScaleName = String
+type Params = M.Map String String
 
 -- | Define the available routes
-data API
+data ChordalAPI
   = Notes
   | Chords
-  | Chord NoteName ChordName
+  | Chord NoteName ChordName Params
   | Scales
-  | Scale NoteName ScaleName
-  {-- | TransposeNotes String --}
+  | Scale NoteName ScaleName Params
+  {-- | TransposeNotes String Params --}
 
 -- | For testing
-instance showAPI :: Show API where 
+instance showAPI :: Show ChordalAPI where 
   show Notes = "Notes"
   show Chords = "Chords"
-  show (Chord a b) = "Chord " <> a <> b
+  show (Chord a b _) = "Chord " <> a <> b
   show Scales = "Scales"
-  show (Scale a b) = "Scale " <> a <> b
-  show _ = "Other API route"
+  show (Scale a b _) = "Scale " <> a <> b
+  {-- show (TransposeNotes a _) = "Transpose " <> a --}
 
 -- -------------------------------
 
+optParams :: Match (M.Map String String)
+optParams = maybe M.empty identity <$> optionalMatch params <* end
+
 -- | Match the URI to the route
-chordalAPI :: Match API
+chordalAPI :: Match ChordalAPI
 chordalAPI = 
   root *> oneOf
   [ Notes  <$ lit "notes"
   , Chords <$ lit "chords"
-  {-- , Chord <$> lit "chord" *> str <*> str --}
+  , Chord <$> (lit "chord" *> str) <*> str <*> optParams
   , Scales <$ lit "scales"
-  {-- , Scale <$> lit "scale" *> str <*> str --}
+  , Scale <$> (lit "scale" *> str) <*> str <*> optParams
+  {-- , TransposeNotes <$> (lit "notes" *> params) <*> optParams --}
   ] <* end
 
 -- -------------------------------
 
-matchAPI :: String -> Either String API
+-- | Convert a URI parameter value to an integer
+stringToInt :: Maybe String -> Int
+stringToInt s = fromMaybe 0 $ s >>= fromString
+
+-- | Convert a list of notes to an array
+stringToArray :: Maybe String -> Array String
+stringToArray s = fromMaybe [] $ S.split (S.Pattern ",") <$> s
+
+-- | Convert URI optional parameters to Chordal options
+options :: Params -> C.Options
+options p = { "transpose": tr, "inversion": inv }
+  where tr = (stringToInt <<< M.lookup "transpose") p
+        inv = (stringToInt <<< M.lookup "inversion") p
+
+
+matchAPI :: String -> Either String ChordalAPI
 matchAPI = match chordalAPI
 
+-- | Route a URI string to a Chordal function
 route :: String -> Effect Unit
-route r = 
-  case m of
-       (Right Notes) -> logShow C.allNotes
-       (Right Chords) -> logShow C.allChords
-       _ -> logShow "Error"
-       where m = matchAPI r
-
-{-- test1 = matchAPI "notes" --}
-{-- test2 = matchAPI "chords" --}
-{-- test3 = matchAPI "chord/C/min" --}
-{-- test4 = matchAPI "chrod/D/abc" --}
-
-
-
+route r = case (matchAPI r) of
+  (Right Notes) -> logShow C.allNotes
+  (Right Chords) -> logShow C.allChords
+  (Right (Chord a b p)) -> logShow $ C.getChord a b $ options p
+  (Right Scales) -> logShow C.allScales
+  (Right (Scale a b p)) -> logShow $ C.getScale a b $ options p
+  {-- (Right (TransposeNotes a p)) -> logShow $ C.transposeNotes $ stringToArray p $ options p --}
+  (Left err) -> logShow "Route error"
 
 -- The End
